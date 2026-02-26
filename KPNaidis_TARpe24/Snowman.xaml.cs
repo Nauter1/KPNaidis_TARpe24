b645ab792769cf1445708e6f7edca34e1ed2f773
opilane <opilane@HP-CZC2349HTM>
2026-02-26 09:46:30 +0200

    Tegin lumememme paremaks

--- KPNaidis_TARpe24/Snowman.xaml.cs
+++ KPNaidis_TARpe24/Snowman.xaml.cs
@@ -6,14 +6,19 @@ namespace KPNaidis_TARpe24;
 
 public partial class Snowman : ContentPage
 {
+    BoxView AmberA;
+    BoxView AmberB;
+    BoxView AmberC;
     Ellipse pallPea;
     Ellipse pallKeha;
     Ellipse pallJalad;
+
     Picker snowpick;
     AbsoluteLayout abs;
 
     public Snowman()
     {
+        
         pallPea = new Ellipse
         {
             WidthRequest = 125,
@@ -22,6 +27,33 @@ public partial class Snowman : ContentPage
             Stroke = Colors.Black,
             StrokeThickness = 5,
             HorizontalOptions = LayoutOptions.Center
+        };
+        AmberA = new BoxView
+        {
+            Color = Color.FromRgb(0, 0, 0),
+            WidthRequest = 40,
+            HeightRequest = 80,
+            HorizontalOptions = LayoutOptions.Center,
+            BackgroundColor = Color.FromRgba(0, 0, 0, 0),
+            CornerRadius = 0,
+        };
+        AmberB = new BoxView
+        {
+            Color = Color.FromRgb(0, 0, 0),
+            WidthRequest = 50,
+            HeightRequest = 30,
+            HorizontalOptions = LayoutOptions.Center,
+            BackgroundColor = Color.FromRgba(0, 0, 0, 0),
+            CornerRadius = 0,
+        };
+        AmberC = new BoxView
+        {
+            Color = Color.FromRgb(127, 0, 255),
+            WidthRequest = 40,
+            HeightRequest = 5,
+            HorizontalOptions = LayoutOptions.Center,
+            BackgroundColor = Color.FromRgba(0, 0, 0, 0),
+            CornerRadius = 0,
         };
         pallKeha = new Ellipse
         {
@@ -43,10 +75,36 @@ public partial class Snowman : ContentPage
         };
         snowpick = new Picker
         {
-            Title = "Snowman Valik"
+            Title = "Snowman Valik",
+            ItemsSource = new List<string> { "Peida", "Näita", "Muuda värv","Sulata","Tantsi","TTS","Ööreþiim" },
+            HorizontalOptions = LayoutOptions.Center
         };
-        abs = new AbsoluteLayout { Children = { pallPea, pallKeha, pallJalad, snowpick } };
+        snowpick.SelectedIndexChanged += (sender, e) =>
+        {
+            switch (snowpick.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+            }
+        };
+        abs = new AbsoluteLayout { Children = { AmberA, AmberB, AmberC, pallPea, pallKeha, pallJalad, snowpick } };
+
         AbsoluteLayout.SetLayoutBounds(pallPea, new Rect(120, 80, 200, 50));
+        AbsoluteLayout.SetLayoutBounds(AmberA, new Rect(120, 30, 200, 50));
+        AbsoluteLayout.SetLayoutBounds(AmberB, new Rect(120, 30, 200, 50));
+        AbsoluteLayout.SetLayoutBounds(AmberC, new Rect(120, 10, 200, 50));
         AbsoluteLayout.SetLayoutBounds(pallKeha, new Rect(120, 170, 200, 50));
         AbsoluteLayout.SetLayoutBounds(pallJalad, new Rect(120, 280, 200, 50));
         AbsoluteLayout.SetLayoutBounds(snowpick, new Rect(120, 420, 200, 50));
